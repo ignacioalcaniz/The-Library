e39d0ea5f6c60c522d312b177582928d7c5ba8bd
ignacio alcaniz <ignaalcaniz@gmail.com>
2024-08-14 15:19:24 -0300

    primer coomit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+    return(
+        <div>
+            <h1 style={{textAlign:"center",backgroundColor:"blue",border:"2px solid black",borderRadius:"10px"}} >The Library</h1>
+        
+            
+        </div>
+        
+
+    ) 
+    
+}
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import App from "./App";
+
+const divPrincipal=document.getElementById("root")
+
+const root=ReactDOM.createRoot(divPrincipal)
+
+
+root.render(<App/>)
